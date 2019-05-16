a3a0e300ce000b3af6da36a20507a6751db9c026
Yujikawa <windbell0404@gmail.com>
2019-05-16 19:08:42 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# bucklogbi
+# BacklogBI
+BacklogのBIツールです
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
